806715b266e24f6f00cdd4b9878118fb57887d7f
Romain Tartière <romain@blogreen.org>
2022-02-28 16:32:18 -1000

    Remove ruby code from puppet manifest
    puts is a Ruby function, not available in Puppet manifests.

--- examples/OK_block.pp
+++ examples/OK_block.pp
@@ -16,7 +16,6 @@ syslog_ng::config { 'block foo':
 
 $_coloss_analyzers = ['a','b','c']
 $_coloss_analyzer_destinations = $_coloss_analyzers.map |$host| { { 'coloss' => "server(${host})" } }
-puts($_coloss_analyzer_destinations)
 ::syslog_ng::log { 'd_check_elasticsearch_roundtrip':
   params => flatten( [
       { 'source'      => 's_check_elasticsearch_roundtrip' },
